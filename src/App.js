114e1ed8cd6f200d9e29ec22c697795a0ce6798f
hana-nazer <hanapp9966@gmail.com>
2022-12-05 12:15:46 +0530

    destructuring in class component

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
       {/* <Message></Message> */}
      {/* <Greet name="hana" heroName="soooper">
       This is children props
      </Greet> */}
-     <Greet name="hanaa" heroName="powefulll"></Greet>
-     <Greet name="hanaaa" heroName="rfghj"></Greet>
+     {/* <Greet name="hanaa" heroName="powefulll"></Greet>
+     <Greet name="hanaaa" heroName="rfghj"></Greet> */}
 
-     {/* <Welcome  name="hanaa" heroName="powefulll"></Welcome>
-     <Welcome name="hana" heroName="soooper"></Welcome>
+     <Welcome  name="hanaa" heroName="powefulll"></Welcome>
+     {/* <Welcome name="hana" heroName="soooper"></Welcome>
      <Welcome name="hanaa" heroName="powefulll"></Welcome> */}
 
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
  class Welcome extends Component{
     render(){
-        return <h1>{this.props.name} aka {this.props.heroName}</h1>
+        const {name,heroName} = this.props
+        return <h1>{name} aka {heroName}</h1>
     }
  }
  export default Welcome
