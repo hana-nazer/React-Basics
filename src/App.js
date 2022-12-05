fd43f4b5d2e2fa4cc6fbc2837c2c0f4318ca251a
hana-nazer <hanapp9966@gmail.com>
2022-12-05 12:09:08 +0530

    destructuring in functional componenet - way1

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
       {/* <Message></Message> */}
      {/* <Greet name="hana" heroName="soooper">
       This is children props
-     </Greet>
+     </Greet> */}
      <Greet name="hanaa" heroName="powefulll"></Greet>
      <Greet name="hanaaa" heroName="rfghj"></Greet>
 
-     <Welcome  name="hanaa" heroName="powefulll"></Welcome>
+     {/* <Welcome  name="hanaa" heroName="powefulll"></Welcome>
      <Welcome name="hana" heroName="soooper"></Welcome>
      <Welcome name="hanaa" heroName="powefulll"></Welcome> */}
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react'
 
- class Counter extends Component {
-    constructor(props){
+class Counter extends Component {
+    constructor(props) {
         super(props)
         this.state = {
-            count : 0
+            count: 0
         }
     }
+    // incrementing the count by one in normal form
+    increment() {
+        //     this.setState({
+        //         count : this.state.count + 1
+        //     }, ()=>{console.log('callback value', this.state.count);})
 
-    increment(){
-        this.setState({
-            count : this.state.count + 1
-        }, ()=>{console.log('callback value', this.state.count);})
+        // imcrementing the count using the previous state
+        this.setState((prevState) => (({
+            count: prevState.count + 1
+        })))
+
     }
-  render() {
-    return (
-      <div>
-          <div>Count- {this.state.count}</div>
-          <button onClick={()=>this.increment()}>Increment</button>
-      </div>
-    )
-  }
+    incrementFive() {
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+    }
+
+    render() {
+        return (
+            <div>
+                <div>Count- {this.state.count}</div>
+                <button onClick={() => this.incrementFive()}>Increment</button>
+            </div>
+        )
+    }
 }
 
 export default Counter
